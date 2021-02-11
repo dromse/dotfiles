18cfd11a16b004de6e2e687f364434249da6aefc
dromse <dromseproducer@gmail.com>
2021-02-11 15:43:50 +0000

    Refactored: added the more understandable structure

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,27 +1,39 @@
+# Fish settings
 set fish_greeting
 
+# Package manager aliases
 alias install "sudo pacman -S"
 alias update "sudo pacman -Syu"
+alias remove "sudo pacman -R"
+
+alias aur "yay -S"
+
+# System aliases
+alias cpumax "sudo cpupower frequency-set --max"
+alias cpumin "sudo cpupower frequency-set --min"
+alias cpuinfo "cpupower frequency-info"
+
+# Text editor aliases
+alias v "nvim"
+alias suv "sudo nvim"
+
+# Change config files aliases
+alias config_alacritty "nvim ~/.config/alacritty/alacritty.yml"
 alias config_fish "nvim ~/.config/fish/config.fish"
 alias config_polybar "nvim ~/.config/polybar/config"
 alias config_awesome "nvim ~/.config/awesome/rc.lua"
-alias remove "sudo pacman -R"
 alias config_qtile "nvim ~/.config/qtile/config.py"
 alias config_nvim "nvim ~/.config/nvim/init.vim"
 alias config_i3 "nvim ~/.config/i3/config"
 alias config_ranger "cd ~/.config/ranger/"
-alias v "nvim"
-alias suv "sudo nvim"
-alias r "ranger"
-alias cpumax "sudo cpupower frequency-set --max"
-alias cpumin "sudo cpupower frequency-set --min"
-alias cpuinfo "cpupower frequency-info"
-alias config_alacritty "nvim ~/.config/alacritty/alacritty.yml"
 alias configs "cd ~/.config"
 alias config_picom "nvim ~/.config/picom/picom.conf"
 alias update_dotfiles "cd ~/dotfiles && git add . && git commit -m"
+
+# Short aliases for freqently uses commands
 alias c "clear"
 alias k "cd .."
 alias j "cd"
-alias ue4 "cd ~/UnrealEngine/Engine/Binaries/Linux/ && ./UE4Editor"
-alias aur "yay -S"
+alias r "ranger"
+
+
