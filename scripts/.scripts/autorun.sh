f9f316159d3a05a23957d3e67460347bfe1a2cc3
dromse <qasx@localhost.localdomain>
2021-04-30 19:29:24 +0300

    update before installing windows

--- scripts/.scripts/autorun.sh
+++ scripts/.scripts/autorun.sh
@@ -2,9 +2,11 @@
 
 killall polybar
 polybar example &
+pgrep -x picom     > /dev/null || picom &
 pgrep -x udiskie   > /dev/null || udiskie --no-automount --no-notify --tray &
 pgrep -x xbindkeys > /dev/null || xbindkeys -p &
-pgrep -x paperview > /dev/null || paperview /usr/share/wallpaper/night 5 &
+# pgrep -x paperview > /dev/null || paperview /usr/share/wallpaper/night 5 &
+pgrep -x feh       > /dev/null || feh --bg-scale ~/Documents/wall.jpg &
 
 # speed of cursor moving
 xset r rate 500 50 &
