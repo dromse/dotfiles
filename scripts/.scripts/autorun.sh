8f3cddb8df7e47e8afcfeb4648f19a91f0667e7a
dromse <dromseproducer@gmail.com>
2021-06-25 15:31:32 +0300

    change wallpaper from random to static

--- scripts/.scripts/autorun.sh
+++ scripts/.scripts/autorun.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+# speed of cursor moving
+xset r rate 500 50 &
+# switch layout and swap caps and esc
+setxkbmap -layout us,ru,ua -option grp:alt_shift_toggle -option caps:swapescape &
+
 killall polybar
 polybar example &
 #pgrep -x picom     > /dev/null || picom &
 #pgrep -x udiskie   > /dev/null || udiskie --no-automount --no-notify --tray &
 pgrep -x xbindkeys > /dev/null || xbindkeys -p &
 # pgrep -x paperview > /dev/null || paperview /usr/share/wallpaper/night 5 &
-pgrep -x feh       > /dev/null || feh --bg-scale ~/.scripts/wall.jpg &
+pgrep -x feh       > /dev/null || feh --bg-fill https://w.wallhaven.cc/full/pk/wallhaven-pkw2x3.jpg 
 
-# speed of cursor moving
-xset r rate 500 50 &
-
-# switch layout and swap caps and esc
-setxkbmap -layout us,ru,ua -option grp:alt_shift_toggle -option caps:swapescape &
